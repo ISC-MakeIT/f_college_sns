47736a12cb13d7bef9265088fe99431ca1ca071f
fumihumi <t.fum1hum@gmail.com>
2018-08-08 13:49:27 +0900

    ダミーデータの生成とそれに伴うfactoryの変更

--- src/typescripts/entities/product.ts
+++ src/typescripts/entities/product.ts
@@ -1,8 +1,12 @@
 export class Product {
     public constructor(
         public id: number,
+        public title: string,
         public owner: string,
         public concept: string,
         public imageURLPath: string,
+        public otherImageURLPath: string[] | null,
+        public promotion: string | null,
+        public members: string[] | null,
     ) {}
 }
--- src/typescripts/services/product.ts
+++ src/typescripts/services/product.ts
@@ -1,18 +1,38 @@
 import { Product } from '../entities';
+import { ApiClient } from '../infrastructure';
+import { ProductFactory } from '../factories';
 
 export class ProductService {
-    public static async get(id: number) {
-        // APIにぶん投げるか
-        // if (this.)
-    }
 
     public static async getAll() {
         // APIClient.get('/products')で全てのプロダクトJSONもらいたい
         const array = [];
         for (let i = 0; i < 3; i++) {
-            const p = new Product( i, `${i}さん`, `this concept is hogehoge`, `assets/images/${i}` );
+            const p = new Product(
+                i,
+                `${i}のための〇〇`,
+                `${i}さん`,
+                `this concept is hogehoge`,
+                `assets/images/${i}`,
+                null, null, null);
             array.push(p);
         }
         return array;
+    }
+
+    public static async get(id: number) {
+        // const ret = ApiClient.get(`api/products/${id}`);
+        // const product = ProductFactory.createFromJSON(ret);
+        const product = new Product(
+            id,
+            '情熱と希望の賛歌',
+            `${id}：斎藤ほのか`,
+            'このサービスは,情熱をテーマに作成しました。細部までこだわって作成しているのでモデルの手足のその先にある情熱を感じていただけたら幸いです。',
+            `public/assets/images/${id}.jpg`,
+            [`public/assets/images/${id}.jpg`, `public/assets/images/${id}.jpg`],
+            '細部へのこだわりのほか、全体的な配色にもこだわりがあります。正面から見ると妖艶な濃厚な赤を、後ろ姿からは真紅な赤を基調とすることでどこから見ても情熱を感じられるような構成となっています。',
+            ['阿部智恵美', '佐々木みなみ', '鍋島恵里奈'],
+        );
+        return product;
     }
 }
