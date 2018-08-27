bdee8a50f1321981b7370641d4ebf2e8fe408061
fumihumi <t.fum1hum@gmail.com>
2018-08-27 13:42:59 +0900

    add 404 page

---
+++ src/typescripts/screens/not_found.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { RouteComponentProps, Link} from 'react-router-dom';
+import Screen from './screen';
+
+interface Props extends RouteComponentProps<{}> {}
+
+export const NotFound: React.StatelessComponent<Props> = () => {
+    return (
+        <Screen name='not-found' hideHeader>
+            <section className='text-center'>
+                <div className='content-title'>
+                    <h1 className='404'>
+                        404
+                    </h1>
+                    <p>PAGE NOT FOUND</p>
+                </div>
+
+                <div className='content-nav'>
+                    <p>お探しのページは</p>
+                    <p>見つかりませんでした</p>
+                </div>
+
+                <Link className='btn btn-light redirect-link' to='/'>
+                    トップページへ戻る
+                </Link>
+            </section>
+        </Screen>
+    );
+};
