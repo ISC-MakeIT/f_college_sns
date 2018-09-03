3449ef715c3f083a8627c9e1e16f992366c253c8
fumihumi <t.fum1hum@gmail.com>
2018-09-04 03:54:38 +0900

    一旦コミット

--- src/typescripts/services/photo.ts
+++ src/typescripts/services/photo.ts
@@ -1,3 +1,5 @@
+import { Constants } from '../infrastructure/constants';
+
 export class PhotoService {
 
     public static buildPhotoPath = (name: string, dir: string) => {
@@ -8,42 +10,61 @@ export class PhotoService {
         return `https://s3-ap-northeast-1.amazonaws.com/f-college-images`;
     }
 
+    public static getS3Image = (type: 'logos' | 'products', name: string) => {
+        const params = {
+            Bucket: 'f-college-images',
+            Key: `${type}/${name}`,
+        };
+
+        Constants.s3Client.getObject(params, (err, data) => {
+            if (err) {
+                console.log(err, err.stack);
+                // debugger
+                return;
+            }
+            console.log(data);
+            // debugger
+        });
+    }
+
     public static buildPhotoPathFromId = (id: number) => {
         const imageNames = [
-            '1_Br2A_SHIOYA_Konatsu',
-            '2_Br2A_TATEHASHI_Yui',
-            '3_Br2A_MARUYAMA_Shiori',
-            '4_Br2B_OONUKI_Erii',
-            '5_Br2B_NEMOTO_Maho',
-            '6_FLD2_KITAZAWA_Saaya',
-            '7_FB1A_NAKANO_Kaito',
-            '8_FB1A_ALABADO_Natasha',
-            '9_FB1B_MIYAUCHI_Haruka',
-            '10_FB2_KAYANUMA_Azumi',
-            '11_FB2_KIMURA_Takaya',
-            '12_FB2_MURAYAMA_Sakura',
-            '13_FLD1_ISHIZAWA_Asahi',
-            '14_FLD1_OKUMA_Aoi',
-            '15_FLD1_SUZUKI_Syota',
-            '16_FLD1_MATSUMURA_Misaki',
-            '17_FLD1_Yamauchi_Ryo',
-            '18_FLD2_KATO_Sotaro',
-            '19_FLD2_GOTO_Amii',
-            '20_FLD2_TAKAHASHI_Ren',
-            '21_FLD2_NARUSE_Kaede',
-            '22_FLD2_ISHIDA_Hinata',
-            '23_FLD2_TAKAHASHI_Kokoro',
-            '24_FLD3_OGAWA_Aimi',
-            '25_FLD3_OSHITARI_Fuka',
-            '26_FLD3_KOHARA_Hiroki',
-            '27_FLD3_NOGUCHI_Aika',
-            '28_FLD3_OGURA_Mariko',
-            '29_FLD3_HIRAMA_Haruki',
-            '30_FLD3_SUZUKI_Minatsu',
+            '1_Br2A_SHIOYA_Konatsu.jpg',
+            '2_Br2A_TATEHASHI_Yui.jpg',
+            '3_Br2A_MARUYAMA_Shiori.jpg',
+            '4_Br2B_OONUKI_Erii.jpg',
+            '5_Br2B_NEMOTO_Maho.jpg',
+            '6_FLD2_KITAZAWA_Saaya.jpg',
+            '7_FB1A_NAKANO_Kaito.jpg',
+            '8_FB1A_ALABADO_Natasha.jpg',
+            '9_FB1B_MIYAUCHI_Haruka.jpg',
+            '10_FB2_KAYANUMA_Azumi.jpg',
+            '11_FB2_KIMURA_Takaya.jpg',
+            '12_FB2_MURAYAMA_Sakura.jpg',
+            '13_FLD1_ISHIZAWA_Asahi.jpg',
+            '14_FLD1_OKUMA_Aoi.jpg',
+            '15_FLD1_SUZUKI_Syota.jpg',
+            '16_FLD1_MATSUMURA_Misaki.jpg',
+            '17_FLD1_Yamauchi_Ryo.jpg',
+            '18_FLD2_KATO_Sotaro.jpg',
+            '19_FLD2_GOTO_Amii.jpg',
+            '20_FLD2_TAKAHASHI_Ren.jpg',
+            '21_FLD2_NARUSE_Kaede.jpg',
+            '22_FLD2_ISHIDA_Hinata.jpg',
+            '23_FLD2_TAKAHASHI_Kokoro.jpg',
+            '24_FLD3_OGAWA_Aimi.jpg',
+            '25_FLD3_OSHITARI_Fuka.jpg',
+            '26_FLD3_KOHARA_Hiroki.jpg',
+            '27_FLD3_NOGUCHI_Aika.jpg',
+            '28_FLD3_OGURA_Mariko.jpg',
+            '29_FLD3_HIRAMA_Haruki.jpg',
+            '30_FLD3_SUZUKI_Minatsu.jpg',
         ];
 
-        const img = imageNames.find(i => Number(i.split('_')[0]) === id);
-        return `${PhotoService.basePhotoPath()}/products/${img}.jpg`;
+        const img = imageNames.find(i => Number(i.split('_')[0]) === id) || '';
+        // ${PhotoService.getS3Image('products', img)}
+        // debugger
+        return `${PhotoService.getS3Image('products', img)}`;
     }
 
     private static basePhotoPath = () => './assets/images';
