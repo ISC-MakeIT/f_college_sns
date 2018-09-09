78faa07a6628214fb8c30476f58438ef085cea97
fumihumi <t.fum1hum@gmail.com>
2018-09-10 00:07:24 +0900

    lintと画像対応

--- src/typescripts/components/header.tsx
+++ src/typescripts/components/header.tsx
@@ -1,24 +1,28 @@
 import * as React from 'react';
-import {withRouter, Link } from 'react-router-dom';
-import { Icon } from '../components';
+import {withRouter, Link} from 'react-router-dom';
+import {Icon} from '../components';
+import { PhotoService } from '../services/photo';
 
 interface Props {
     showBackButton?: boolean;
 }
 
-export class Header extends React.Component<Props, {}> {
+export class Header extends React.Component < Props, {} > {
 
     public render() {
-
         return (
             <header id='header_wrap' className='component'>
                 {this.props.showBackButton ? <BackButton/> : null}
                 <Link to='/' className='header-logo'>
-                    <img src='./assets/images/top/logo.svg' alt='header-logo' className='header_logo'/>
+                <img
+                    alt='header-logo'
+                    className='header_logo'
+                    src={PhotoService.getS3PhotoPath('logo.svg', 'logos')}
+                />
                 </Link>
                 <Link to='/votes'>
                     <div id='header_label'>
-                        <Icon name='crown' />
+                        <Icon name='crown'/>
                         <span>あと５票</span>
                         <div className='label_push'>5</div>
                     </div>
@@ -28,6 +32,4 @@ export class Header extends React.Component<Props, {}> {
     }
 }
 
-const BackButton = withRouter(props => (
-    <div className='back-arrow' onClick={() => props.history.goBack()}/>
-));
+const BackButton = withRouter(props => (<div className='back-arrow' onClick={() => props.history.goBack()}/>));
