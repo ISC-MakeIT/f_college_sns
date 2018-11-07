61b12c8c1ae68deedd0072473013e991530c644d
fumihumi <t.fum1hum@gmail.com>
2018-11-07 20:23:02 +0900

    リファクタ

--- src/typescripts/services/initialization.ts
+++ src/typescripts/services/initialization.ts
@@ -1,115 +1,56 @@
 export class InitializationService {
     public static async initialize() {
         try {
-            this.detectPrivateMode(
-                (isPrivate: any) => {
-                    // tslint:disable-next-line:no-unused-expression
-                    typeof isPrivate === 'undefined' ? 'cannot detect' : isPrivate ? 'private' : 'not private';
-                },
-            );
-
+            this.isPrivateMode().then((isPrivate) => {
+                console.log('Is in private mode: ', isPrivate);
+            });
         // tslint:disable-next-line:no-empty
         } catch (e) { }
         return;
     }
 
-    private static async detectPrivateMode(callback: any) {
-        let isPrivate: any;
-
-        if (window.webkitRequestFileSystem) {
-            window.webkitRequestFileSystem(
-                window.TEMPORARY, 1,
-                () => {
-                    isPrivate = false;
-                },
-                (e: any) => {
-                    // tslint:disable-next-line:no-console
-                    console.log(e);
-                    isPrivate = true;
-                },
-            );
-        } else if (window.indexedDB && /Firefox/.test(window.navigator.userAgent)) {
-            let db: any;
-            try {
-                db = window.indexedDB.open('test');
-            } catch (e) {
-                isPrivate = true;
-            }
-
-            if (typeof isPrivate === 'undefined') {
-                this.retry(
-                    function isDone() {
-                        return db.readyState === 'done' ? true : false;
-                    },
-                    function next(isTimeout: any) {
-                        if (!isTimeout) {
-                            isPrivate = db.result ? false : true;
-                        }
-                    },
-                );
-            }
-        } else if (this.isIE10OrLater(window.navigator.userAgent)) {
-            isPrivate = false;
-            try {
-                if (!window.indexedDB) {
-                    isPrivate = true;
+    private static async isPrivateMode() {
+        return new Promise(resolve => {
+            const on = () => resolve(true); // is in private mode
+            const off = () => resolve(false); // not private mode
+            const testLocalStorage = () => {
+                try {
+                    if (localStorage.length) off();
+                    else {
+                        localStorage.x = 1;
+                        localStorage.removeItem('x');
+                        off();
+                    }
+                } catch (e) {
+                    // Safari only enables cookie in private mode
+                    // if cookie is disabled then all client side storage is disabled
+                    // if all client side storage is disabled, then there is no point
+                    // in using private mode
+                    navigator.cookieEnabled ? on() : off();
                 }
-            } catch (e) {
-                isPrivate = true;
+            };
+            // Chrome & Opera
+            if (window.webkitRequestFileSystem) {
+                return void window.webkitRequestFileSystem(0, 0, off, on);
             }
-        } else if (window.localStorage && /Safari/.test(window.navigator.userAgent)) {
-            try {
-                window.localStorage.setItem('test', '1');
-            } catch (e) {
-                isPrivate = true;
+            // Firefox
+            if ('MozAppearance' in document.documentElement.style) {
+                const db = indexedDB.open('test');
+                db.onerror = on;
+                db.onsuccess = off;
+                return void 0;
             }
-
-            if (typeof isPrivate === 'undefined') {
-                isPrivate = false;
-                window.localStorage.removeItem('test');
+            // Safari
+            if (/constructor/i.test(window.HTMLElement)) {
+                return testLocalStorage();
             }
-        }
-
-        this.retry(
-            function isDone() {
-                return typeof isPrivate !== 'undefined' ? true : false;
-            },
-            function next(isTimeout: any) {
-                callback(isPrivate);
-            },
-        );
+            // IE10+ & Edge
+            if (!window.indexedDB && (window.PointerEvent || window.MSPointerEvent)) {
+                return on();
+            }
+            // others
+            return off();
+        });
     }
-
-    private static async retry(isDone: any, next: any) {
-        let currentTrial = 0;
-        let isTimeout = false;
-        const maxRetry = 50;
-        const interval = 10;
-        const id = window.setInterval(() => {
-                    if (isDone()) {
-                        window.clearInterval(id);
-                        next(isTimeout);
-                    }
-                    if (currentTrial++ > maxRetry) {
-                        window.clearInterval(id);
-                        isTimeout = true;
-                        next(isTimeout);
-                    }
-                },
-                interval,
-            );
-    }
-
-    private static async isIE10OrLater(userAgent: any) {
-            const ua = userAgent.toLowerCase();
-            if (ua.indexOf('msie') === 0 && ua.indexOf('trident') === 0) {
-                return false;
-            }
-            const match = /(?:msie|rv:)\s?([\d\.]+)/.exec(ua);
-            if (match && parseInt(match[1], 10) >= 10) {
-                return true;
-            }
-            return false;
-        }
 
 }
