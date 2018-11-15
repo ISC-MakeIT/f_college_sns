f79e516ecd67b8325c348d54026224aa23cfd0cc
fumihumi <t.fum1hum@gmail.com>
2018-11-16 03:11:16 +0900

    index用のProductクラス

---
+++ src/typescripts/factories/product_list.ts
@@ -0,0 +1,30 @@
+import { UserFactory } from './user';
+import { ProductList, User } from '../entities';
+
+type ProductType = 'fashion' | 'beauty';
+interface OwnerProps {
+    student_id: number;
+    student_name: string;
+    student_class: string;
+    profile_photo: string;
+    leader_flg: number | null;
+}
+
+interface ProductProps {
+    product_id: number;
+    entry_order: number;
+    product_number: number;
+    head_shot: string;
+    owner: OwnerProps;
+}
+
+export class ProductListFactory {
+    public static createFromJSON(p: ProductProps) {
+        return new ProductList(
+            p.product_id,
+            p.entry_order,
+            p.head_shot,
+            UserFactory.createFromJSON(p.owner),
+        );
+    }
+}
