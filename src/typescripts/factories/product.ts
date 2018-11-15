2925cccb8f70be80c1f4917d54d45e1b513485b5
fumihumi <t.fum1hum@gmail.com>
2018-11-16 03:10:26 +0900

    productsまわり

--- src/typescripts/factories/product.ts
+++ src/typescripts/factories/product.ts
@@ -1,18 +1,26 @@
-import { Product } from '../entities';
+import { Product, User } from '../entities';
 import { UserFactory } from './user';
+import { PhotoService } from '../services/photo';
+
+type ProductType = 'fashion' | 'beauty';
+interface ProductJsonType {
+    product_id: number;
+    genre: ProductType;
+    theme: string;
+    concept: string;
+    members: User[];
+    photos: string[];
+}
 
 export class ProductFactory {
-    public static createFromJSON(product: any) {
-        const owner = UserFactory.createFromJSON(product.owner);
+    public static createFromJSON(p: ProductJsonType ) {
         return new Product(
-            product.id,
-            product.title,
-            owner,
-            product.concept,
-            product.imageURLPath,
-            product.otherImageURLPath,
-            product.promotion,
-            product.members,
+            p.product_id,
+            p.genre,
+            p.theme,
+            p.concept,
+            p.members.map(m => UserFactory.createFromJSON(m)),
+            p.photos.map(p => PhotoService.getS3PhotoPath(p)),
         );
     }
 }
