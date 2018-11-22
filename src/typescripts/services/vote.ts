6246f81725b27d5f3074e3c32c82ac6a2d847f0f
fumihumi <t.fum1hum@gmail.com>
2018-11-22 15:07:23 +0900

    投票呼び出し関数。判断用の関数を作成

--- src/typescripts/services/vote.ts
+++ src/typescripts/services/vote.ts
@@ -1,32 +1,41 @@
 import { ApiClient } from '../infrastructure';
 import { ApplicationManager } from '../application_manager';
-import { ProductType } from '../entities';
+import { ProductType, Product } from '../entities';
 
 export class VoteService {
     public static async vote(method: 'POST' | 'DELETE', productId: number, genre: ProductType) {
-        if (!this.canIncrement(genre)) return;
         if (method === 'POST') {
-            // this.increment(productId);
+            this.increment(productId, genre);
         } else {
-            // this.decrement(productId);
+            this.decrement(productId, genre);
         }
     }
 
-    private static canIncrement(genre: ProductType) {
+    public static canIncrement(genre: ProductType) {
         const appManager = ApplicationManager.instance;
         const maxVoteCount = genre.toLowerCase() === 'fashion' ? ApplicationManager.FASHION_VOTE_COUNT : ApplicationManager.BEAUTY_VOTE_COUNT;
         const voteIds = appManager.voteIds[genre.toLowerCase()];
 
-        return maxVoteCount > voteIds.length - 1 ? true : false;
+        const beVoter = maxVoteCount > voteIds.length ? true : false;
+        return beVoter;
     }
 
-    private static increment(productId: number) {
-        console.log('increment');
-        // ApiClient.post(`/vote/${productId}`);
+    public static includeVoteId(product: Product) {
+        const appManager = ApplicationManager.instance;
+        const type = product.genre.toLowerCase() === 'fashion' ? 'fashion' : 'beauty';
+
+        return appManager.voteIds[type].includes(product.productId);
     }
 
-    private static decrement(productId: number) {
-        console.log('decrement');
-        // ApiClient.delete(`/vote/${productId}`);
+    private static async increment(productId: number, genre: ProductType) {
+        await ApiClient.post(`/vote/${productId}`);
+        const appManager = ApplicationManager.instance;
+        appManager.pushVoteIds(productId, genre);
+    }
+
+    private static async decrement(productId: number, genre: ProductType) {
+        await ApiClient.delete(`/vote/${productId}`);
+        const appManager = ApplicationManager.instance;
+        appManager.popVoteIds(productId, genre);
     }
 }
